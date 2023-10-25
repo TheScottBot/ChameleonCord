6857159f922abb6814c6e5e28fe3a0bbe72b17fe
Scott <s.p.tippett@gmail.com>
2023-10-25 17:51:54 +0200

    More cleanup

--- cogs/chameleon.py
+++ cogs/chameleon.py
@@ -31,24 +31,17 @@ from models import Player, Game
 
 
 class Example(commands.Cog):
-    __slots__ = ("bot", "__game")
+    __slots__ = ("bot", "__game", "__current_colour", "__first_round")
 
     def __init__(self, bot):
         self.bot = bot
         self.__game: Game = Game(players=[], player_deck=player_deck.DECK, category_deck=category_deck.DECK)
+        self.__current_colour = "green"
+        self.__first_round = True
 
     @commands.Cog.listener()
     async def on_message(self, message):
         print(message.content)
-
-    @commands.command(name="cdraw")
-    async def print(self, ctx, *args):
-        card = random.choice(self.__game.category_deck.cards)
-        print(len(self.__game.category_deck.cards))
-        print(card.title)
-
-        self.__game.category_deck.cards.remove(card)
-        await ctx.channel.send(card.categories_formatted())
 
     @commands.command(name="register")
     async def register_player(self, ctx, *args):
@@ -60,8 +53,15 @@ class Example(commands.Cog):
         self.__game.players.append(new_player)
         await ctx.channel.send(f'Player "{new_player.name}" has been added to the game')
 
-    @commands.command(name="pdraw")
-    async def draw_player_cards(self, ctx, colour: str, *args):
+    async def draw_category_card(self, ctx):
+        card = random.choice(self.__game.category_deck.cards)
+        print(len(self.__game.category_deck.cards))
+        print(card.title)
+
+        self.__game.category_deck.cards.remove(card)
+        await ctx.channel.send(card.categories_formatted())
+
+    async def draw_player_cards(self, colour: str):
         chameleon = random.choice(self.__game.players)
         # do this better as it sucks
         for i in range(len(self.__game.players)):
@@ -75,16 +75,27 @@ class Example(commands.Cog):
 
     @commands.command(name="next")
     async def next_round_setup(self, ctx, *args):
-        for i in range(len(self.__game.players)):
-            if self.__game.players[i].is_chameleon:
-                await ctx.channel.send(f'{self.__game.players[i].name} was the chameleon')
-            self.__game.players[i].is_chameleon = False
+        if not self.__first_round:
+            for i in range(len(self.__game.players)):
+                if self.__game.players[i].is_chameleon:
+                    await ctx.channel.send(f'{self.__game.players[i].name} was the chameleon')
+                self.__game.players[i].is_chameleon = False
+            self.__first_round = False
+        self.assign_colour()
+        await self.draw_player_cards(self.__current_colour)
+        await self.draw_category_card(ctx)
 
     def player_in_game(self, player_id: str) -> bool:
         if any(player.id == player_id for player in self.__game.players):
             return True
         return False
 
-    
+    def assign_colour(self):
+        if self.__current_colour == "green":
+            self.__current_colour = "blue"
+        else:
+            self.__current_colour = "green"
+
+
 async def setup(bot):
     await bot.add_cog(Example(bot))
